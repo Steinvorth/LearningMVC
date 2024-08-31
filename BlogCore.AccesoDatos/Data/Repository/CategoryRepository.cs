7b122b6de7b37cc652aa8e4bb0224e8c5d9916e3
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-08-30 20:23:09 -0600

    Category interface, and repository

---
+++ BlogCore.AccesoDatos/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Models;
+using ProjectoCursoWeb_BlogCore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Category.FirstOrDefault(s => s.Id == category.Id);
+            objFromDb.Name = category.Name;
+            objFromDb.Order = category.Order;
+
+            _db.SaveChanges();
+        }
+    }
+}
--- BlogCore.Models/Category.cs
+++ BlogCore.Models/Category.cs
@@ -14,7 +14,7 @@ namespace BlogCore.Models
 
         [Required(ErrorMessage = "A category name is required!")] // This is the message that will be displayed if the user tries to submit the form without entering a category name
         [Display(Name = "Category Name")] // This is the name that will be displayed in the form
-        public string Nombre { get; set; }
+        public string Name { get; set; }
 
 
         [Display(Name = "Order View")]
