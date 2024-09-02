f4cc1b094e371fd3a89402de692655e6253c9622
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-09-02 14:27:32 -0600

    Item Model

--- BlogCore.Models/Category.cs
+++ BlogCore.Models/Category.cs
@@ -12,12 +12,14 @@ namespace BlogCore.Models
         [Key]
         public int Id { get; set; }
 
+        //Name
         [Required(ErrorMessage = "A category name is required!")] // This is the message that will be displayed if the user tries to submit the form without entering a category name
         [Display(Name = "Category Name")] // This is the name that will be displayed in the form
         public string Name { get; set; }
 
-
+        //order
         [Display(Name = "Order View")]
+        [Range(1,100, ErrorMessage="The value must be between 1 and 100!")]
         public int? Order { get; set; }
 
 
