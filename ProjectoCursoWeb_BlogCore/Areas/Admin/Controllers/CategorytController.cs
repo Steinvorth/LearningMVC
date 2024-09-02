37b935ed70ef6a751da1973809c31149d1de48f7
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-09-02 10:32:51 -0600

    Create CategorytController.cs

---
+++ ProjectoCursoWeb_BlogCore/Areas/Admin/Controllers/CategorytController.cs
@@ -0,0 +1,20 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectoCursoWeb_BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")] //This is the area, its needed to identify the controller
+    public class CategorytController : Controller
+    {
+        private readonly IWorkContainer _workContainer;
+
+        public CategorytController(IWorkContainer workContainer)
+        {
+            _workContainer = workContainer;
+        }        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
